ecf607f0f0aae3c8f873f006d24c93020a8b9186
daisuke-kakuma <daisuke.kakuma@woven-planet.global>
2023-12-08 11:38:13 +0900

    Submit challenge for grading

---
+++ 01_introduction_to_cpp/01_challenge/main.cpp
@@ -0,0 +1,13 @@
+// 不足している値、データ型、その他のコードを追加してプログラムを完成させてください。
+#include <iostream>
+
+int main() {
+    const int myNumber{0};
+
+    const int anotherNumber{5};
+
+    const int answer{myNumber + anotherNumber};
+
+    std::cout << answer << std::endl;
+    return 0;
+}
